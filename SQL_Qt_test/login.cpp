df47e94d43de924ac390f0fba9fe11f8067e27dc
Alexandr <muga.alexandr@yandex.ru>
2019-06-05 01:12:41 +0300

    Test

---
+++ SQL_Qt_test/login.cpp
@@ -0,0 +1,37 @@
+#include "login.h"
+#include "ui_login.h"
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+Login::Login(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Login)
+{
+    parent->setParent()
+    ui->setupUi(this);
+    ui->lineEdit_4->setEchoMode(QLineEdit::Password);
+    mes = new QMessageBox();
+}
+
+Login::~Login()
+{
+    delete ui;
+}
+
+void Login::on_pushButton_clicked()
+{
+    db = QSqlDatabase::addDatabase("QODBC");
+    db.setDatabaseName(QString("DRIVER={SQL Server};SERVER=%1;DATABASE=%2;")
+                       .arg(ui->lineEdit->text())
+                       .arg(ui->lineEdit_2->text()));
+    db.setUserName(ui->lineEdit_3->text());
+    db.setPassword(ui->lineEdit_4->text());
+
+    if (db.open()){
+        mes->setText("Succes connect");
+    } else {
+        mes->setText("Fail connected");
+    }
+
+    mes->show();
+}
